847f3d53e323ca810f9136df7ce595ba85916033
Jelvys Triana <105645974+mowgliph@users.noreply.github.com>
2024-08-05 15:45:14 -0400

    add button components

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import Button from './components/buttons/button';
+
+function App() {
+  return (<>
+    Bot en desarrollo
+    <Button title={'Test'} disable={false}/>
+  </>);
+}
+
+export default App;
