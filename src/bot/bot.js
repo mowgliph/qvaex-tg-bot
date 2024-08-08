3f0ca849274bef4d1665fd98838d0533f2d82215
Jelvys Triana <105645974+mowgliph@users.noreply.github.com>
2024-08-08 09:49:00 -0400

    remove old project

--- src/bot/bot.js
+++
@@ -1,9 +0,0 @@
-const { Telegraf } = require('telegraf')
-const { message } = require('telegraf/filters')
-
-const bot = new Telegraf(process.env.BOT_TOKEN)
-bot.start((ctx) => ctx.reply('Welcome'))
-bot.on(message('sticker'), (ctx) => ctx.reply('👍'))
-bot.hears('hi', (ctx) => ctx.reply('Hey there'))
-
-bot.launch()
